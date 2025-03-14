79be614fd2e51bc3bc4536258089413abb924ec5
KanNattawat <nattawat26907@gmail.com>
2025-03-14 20:24:00 +0700

    l merge

--- myapp/src/routes/Gallery.svelte
+++ myapp/src/routes/Gallery.svelte
@@ -192,6 +192,7 @@
       }
     }
   }
+}
 
   async function fetchImage() {
     try {
@@ -224,20 +225,20 @@
   if (nav) nav.style.visibility = "hidden";
 
   document.documentElement.requestFullscreen();
-}
-
-function stopSlideshow() {
-  showSlideshow = false;
-  clearInterval(interval);
-
-  // Show the navbar when exiting slideshow
-  const nav = document.getElementById("naver");
-  if (nav) nav.style.visibility = "visible";
-
-  if (document.fullscreenElement) {
-    document.exitFullscreen();
   }
-}
+
+  function stopSlideshow() {
+    showSlideshow = false;
+    clearInterval(interval);
+
+    // Show the navbar when exiting slideshow
+    const nav = document.getElementById("naver");
+    if (nav) nav.style.visibility = "visible";
+
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    }
+  }
 
 
   function handleImageClick() {
@@ -247,16 +248,27 @@ function stopSlideshow() {
   onMount(async () => {
     await fetchImage();
   });
+
 </script>
 
-<button on:click={startSlideshow} class="bg-gray-700 text-white px-4 py-2 rounded">Fullscreen Slideshow</button>
+<button
+  on:click={startSlideshow}
+  class="bg-gray-700 text-white px-4 py-2 rounded">Fullscreen Slideshow</button
+>
 
 <div class="masonry p-5">
   {#each $images as img (img.id)}
     <div
       class="cursor-pointer transition-transform transform hover:scale-105"
       on:click={() =>
-        openImage(img.url, img.id, img.name, img.description, img.category, img.owner_email)}
+        openImage(
+          img.url,
+          img.id,
+          img.name,
+          img.description,
+          img.category,
+          img.owner_email,
+        )}
     >
       <img
         class="w-full h-auto rounded-lg"
@@ -268,88 +280,118 @@ function stopSlideshow() {
 </div>
 
 {#if selectedImage}
-<div class="fixed inset-0 bg-black bg-opacity-80 flex flex-col items-center overflow-auto" transition:fade={{ duration: 100 }}>
-  <p class="absolute top-25 left-10 text-white text-xl rounded cursor-pointer p-5 font-bold" on:click={closePreview}>
-    Close
-  </p>
-  
-  <img 
-  class="w-fit pt-25 mt-4 cursor-pointer pt-0" 
-  src={selectedImage.url} 
-  alt="Preview" 
-  id="pre_img" 
-  on:click={() => toggleFullscreen()} 
-/>
-
-
-  <div class="bg-gradient-to-br from-gray-900 to-gray-800 text-white rounded-lg w-full p-25 mt-5">
-    <h3 class="text-4xl font-bold inline">{selectedImage.name}</h3>
-    
-    <a href={selectedImage.url} download={selectedImage.name}>
-      <button class="absolute right-15 bg-blue-800 h-10 w-30 rounded-lg hover:bg-blue-900">
-        Download
-      </button>
-    </a>
-    
-    {#if $user && $user.email === selectedImage.owner_email} <!-- Check ownership -->
-    <button class="absolute right-81 bg-yellow-700 h-10 w-30 rounded-lg hover:bg-yellow-800" on:click={handleEditImage}>
-      Edit
-    </button>
-    <button class="absolute right-48 bg-red-800 h-10 w-30 rounded-lg hover:bg-red-900" on:click={handleDeleteImage}>
-      Delete
-    </button>
-    {/if}
-    
-    <!-- Move Favorite button below Download button -->
-    <button class="absolute right-15 bg-gray-800 rounded-lg hover:bg-gray-900 mt-16" on:click|stopPropagation={() => toggleFavorite(selectedImageId)}>
-      {#if favorites.has(selectedImageId)}
-        ❤️ Favorite
-      {:else}
-        🤍 Add To Favorite
-      {/if}
-    </button>
-
-    <p class="text-gray-300 pt-6 text-l">
-      Category <span class="font-semibold">{selectedImage.category}</span>
+  <div
+    class="fixed inset-0 bg-black bg-opacity-80 flex flex-col items-center overflow-auto"
+    transition:fade={{ duration: 100 }}
+  >
+    <p
+      class="absolute top-25 left-10 text-white text-xl rounded cursor-pointer p-5 font-bold"
+      on:click={closePreview}
+    >
+      Close
     </p>
-    
-    <hr class="my-4 border-gray-600" />
-    <p class="leading-relaxed text-xl">{selectedImage.description}</p>
+
+    <img
+      class="w-fit pt-25 mt-4 cursor-pointer pt-0"
+      src={selectedImage.url}
+      alt="Preview"
+      id="pre_img"
+      on:click={() => toggleFullscreen()}
+    />
+
+    <div
+      class="bg-gradient-to-br from-gray-900 to-gray-800 text-white rounded-lg w-full p-25 mt-5"
+    >
+      <h3 class="text-4xl font-bold inline">{selectedImage.name}</h3>
+
+      <a href={selectedImage.url} download={selectedImage.name}>
+        <button
+          class="absolute right-15 bg-blue-800 h-10 w-30 rounded-lg hover:bg-blue-900"
+        >
+          Download
+        </button>
+      </a>
+
+      {#if $user && $user.email === selectedImage.owner_email}
+        <!-- Check ownership -->
+        <button
+          class="absolute right-81 bg-yellow-700 h-10 w-30 rounded-lg hover:bg-yellow-800"
+          on:click={handleEditImage}
+        >
+          Edit
+        </button>
+        <button
+          class="absolute right-48 bg-red-800 h-10 w-30 rounded-lg hover:bg-red-900"
+          on:click={handleDeleteImage}
+        >
+          Delete
+        </button>
+      {/if}
+
+      <!-- Move Favorite button below Download button -->
+      <button
+        class="absolute right-15 bg-gray-800 rounded-lg hover:bg-gray-900 mt-16"
+        on:click|stopPropagation={() => toggleFavorite(selectedImageId)}
+      >
+        {#if favorites.has(selectedImageId)}
+          ❤️ Favorite
+        {:else}
+          🤍 Add To Favorite
+        {/if}
+      </button>
+
+      <p class="text-gray-300 pt-6 text-l">
+        Category <span class="font-semibold">{selectedImage.category}</span>
+      </p>
+
+      <hr class="my-4 border-gray-600" />
+      <p class="leading-relaxed text-xl">{selectedImage.description}</p>
 
       <hr class="my-4 border-gray-600" />
       <div class="mt-5">
         <h3 class="text-3xl font-semibold">Comments</h3>
-        <br>
+        <br />
         {#each comments as comment}
           <p class="mt-2 text-xl">
             ▪ {comment.comment}
-            <span class="text-gray-500 ml-2">{new Date(comment.created_at).toLocaleDateString("en-CA")}</span>
+            <span class="text-gray-500 ml-2"
+              >{new Date(comment.created_at).toLocaleDateString("en-CA")}</span
+            >
           </p>
         {/each}
-        <br>
+        <br />
       </div>
       {#if $user}
-      <textarea class="w-full p-2 mt-3 border rounded bg-gray-800 text-white placeholder-gray-400" bind:value={commentText} placeholder="Add comment ..."></textarea>
-      <button class="mt-2 px-4 py-2 bg-blue-500 text-white rounded" on:click={handleAddComment}>Add Comment</button>
+        <textarea
+          class="w-full p-2 mt-3 border rounded bg-gray-800 text-white placeholder-gray-400"
+          bind:value={commentText}
+          placeholder="Add comment ..."
+        ></textarea>
+        <button
+          class="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
+          on:click={handleAddComment}>Add Comment</button
+        >
       {:else}
-        <textarea class="w-full p-2 mt-3 border rounded bg-gray-800 text-white placeholder-gray-400 cursor-text" placeholder="Please login before adding comment ..." disabled></textarea>
+        <textarea
+          class="w-full p-2 mt-3 border rounded bg-gray-800 text-white placeholder-gray-400 cursor-text"
+          placeholder="Please login before adding comment ..."
+          disabled
+        ></textarea>
       {/if}
     </div>
   </div>
 {/if}
 
-
 {#if showSlideshow}
   <div class="fixed inset-0 bg-black flex items-center justify-center">
     {#if img.length > 0}
-    <img 
-    src={img[currentIndex]} 
-    alt="Slideshow image" 
-    class="max-w-full max-h-full object-contain"
-    on:click={handleImageClick}
-    transition:fade={{ duration: 500 }}
-  />
-  
+      <img
+        src={img[currentIndex]}
+        alt="Slideshow image"
+        class="max-w-full max-h-full object-contain"
+        on:click={handleImageClick}
+        transition:fade={{ duration: 500 }}
+      />
     {:else}
       <p class="text-white">Loading images...</p>
     {/if}
