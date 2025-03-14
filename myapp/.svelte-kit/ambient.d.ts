4467af3c954edc1320309044084b2648702a1c13
KanNattawat <nattawat26907@gmail.com>
2025-03-14 20:00:33 +0700

    l2

--- myapp/.svelte-kit/ambient.d.ts
+++ myapp/.svelte-kit/ambient.d.ts
@@ -37,8 +37,10 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_10284: string;
+	export const EFC_15400: string;
 	export const ERLANG_HOME: string;
+	export const FPS_BROWSER_APP_PROFILE_STRING: string;
+	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GIT_ASKPASS: string;
 	export const HOME: string;
 	export const HOMEDRIVE: string;
@@ -148,8 +150,10 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_10284: string;
+		EFC_15400: string;
 		ERLANG_HOME: string;
+		FPS_BROWSER_APP_PROFILE_STRING: string;
+		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GIT_ASKPASS: string;
 		HOME: string;
 		HOMEDRIVE: string;
